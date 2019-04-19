960a749a104b43f5b28c779e9691df70661c17d3
Sunghun Lee <44564402+shnlee-ds@users.noreply.github.com>
2019-04-19 08:14:30 -0400

    Create Webscraping_OAE.R

---
+++ Web Scraping/OAE/Webscraping_OAE.R
@@ -0,0 +1,33 @@
+setwd("/Users/shnlee/Desktop/RA")
+library(tidyverse)
+library(rvest)
+library(dplyr)
+
+rm(list=ls())
+
+#### Get the table from 
+url = 'http://sparql.hegroup.org/sparql?default-graph-uri=&query=%23To+find+all+OAE+terms+with+mapped+MEDDRA+ID%0D%0A%0D%0APREFIX+obo-term%3A+%3Chttp%3A%2F%2Fpurl.obolibrary.org%2Fobo%2F%3E%0D%0ASELECT+DISTINCT+%3Fx+%3Flabel+%3Fmeddra%0D%0Afrom+%3Chttp%3A%2F%2Fpurl.obolibrary.org%2Fobo%2Fmerged%2FOAE%3E%0D%0AWHERE%0D%0A%7B%0D%0A%3Fx+rdfs%3Alabel++%3Flabel.%0D%0A%3Fx+obo-term%3AOAE_0004334+%3Fmeddra.%0D%0A%7D%0D%0A&format=text%2Fhtml&timeout=0&debug=on'
+webpage = read_html(url)
+node = html_nodes(webpage, "td")
+text = as.character(html_text(node))
+
+table = data.frame(matrix(NA, nrow = length(text)/3, ncol=3))
+colnames(table) = c('url','Name','ID')
+
+for(i in 0:(length(text)/3-1)){
+  table[i+1,1] = text[3*i+1]
+  table[i+1,2] = gsub('\\"', replacement='', x = text[3*i+2])
+  table[i+1,3] = str_extract(text[3*i+3],'100[0-9]*')
+}
+
+write.csv(table, file="webpage2.csv", row.names=F)
+
+#### Duplicates
+
+dup_idx = duplicated(table$Name)
+dup_names = table[dup_idx,'Name']
+
+dup_row_idx = table$Name %in% dup_names
+duplicates = table[dup_row_idx,]
+
+write.csv(duplicates, 'OAE_duplicate_list.csv', row.names = F)
