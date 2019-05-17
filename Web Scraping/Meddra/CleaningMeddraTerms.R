d2a7d0bea109a72935dc1577ce9d459f5b4f9c4f
Sunghun Lee <44564402+shnlee-ds@users.noreply.github.com>
2019-05-17 03:35:56 -0400

    Update CleaningMeddraTerms.R

--- Web Scraping/Meddra/CleaningMeddraTerms.R
+++ Web Scraping/Meddra/CleaningMeddraTerms.R
@@ -1,110 +1,93 @@
 rm(list=ls())
-options(stringsAsFactors = FALSE)
-setwd("/Users/shnlee/Desktop/RA")
-library(tidyverse)
-library(rvest)
+options(stringsAsFactors = FALSE) 
+library(reshape2)
 library(dplyr)
-library(jsonlite)
+
+#----------------------------------------------------------------
+# upload VAX, AE and pat data from VAERS
+# clean and merge them 
+# Output: full_dds.csv
+#----------------------------------------------------------------
+setwd("/Users/shnlee/Desktop/RA")
+
+year=2000:2018
+
+#### data_PAT
+dd_pat=NULL
+for (k in 1:length(year)){
+  tmp=read.csv(paste('VAERS data/',year[k],"VAERSDATA",".csv",sep=""))
+  dd_pat[[k]]=tmp[,c("VAERS_ID","AGE_YRS","CAGE_YR","CAGE_MO","NUMDAYS","SEX","STATE")]
+}
+
+data_PAT=Reduce("rbind",dd_pat)
+data_PAT=data_PAT[order(data_PAT$VAERS_ID),]
+
+#-- clean the age variable (CAGE= CAGE_YR+CAGE_MO by vax_date-birthdate; NUMDAYS:ONSET_DATE-VAX_DATE)
+CAGE_MO=ifelse(is.na(data_PAT$CAGE_MO),0,data_PAT$CAGE_MO)
+CAGE=data_PAT$CAGE_YR+CAGE_MO
+data_PAT$AGE=ifelse(is.na(CAGE), data_PAT$AGE_YRS, CAGE)
+
+#sum(!is.na(data_PAT$AGE_YRS) & is.na(CAGE))
+#sum(abs(data_PAT$AGE_YRS-CAGE)>5,na.rm=TRUE)
+
+#### data_VAC
+dd_vac=NULL
+for (k in 1:length(year)){
+  tmp=read.csv(paste('VAERS data/',year[k],"VAERSVAX",".csv",sep=""))
+  vac.num=aggregate(x = tmp$VAERS_ID, by = list(tmp$VAERS_ID), FUN = "length", drop = FALSE)
+  names(vac.num) = c("VAERS_ID", "vac_num")
+  tmp = merge(tmp, vac.num, by="VAERS_ID")
+  dd_vac[[k]]=tmp[,c("VAERS_ID","VAX_NAME","VAX_TYPE", "vac_num")]
+  dd_vac[[k]]$year=year[k]
+}
+
+data_VAC=Reduce("rbind",dd_vac)
+data_VAC=data_VAC[order(data_VAC$VAERS_ID),]
 
 
-#### Getting all of the AE names from VAERS data
+#### data_AE
 dd_ae=NULL
-year = 2000:2018
 for (k in 1:length(year)){
   tmp=read.csv(paste('VAERS data/', year[k],"VAERSSYMPTOMS",".csv",sep=""))
   tmp=tmp[,c("VAERS_ID","SYMPTOM1","SYMPTOM2","SYMPTOM3","SYMPTOM4","SYMPTOM5")]
   tmp[tmp==""] = NA
-  tmp.long=melt(tmp, id.vars = c('VAERS_ID'), measure.vars = 2:6, na.rm = T, value.name = "AE_NAME")
-  dd_ae[[k]]=tmp.long
+  tmp=melt(tmp, id.vars = c('VAERS_ID'), measure.vars = 2:6, na.rm = T, value.name = "AE_NAME")
+  ae.num = aggregate(x = tmp$VAERS_ID, by = list(tmp$VAERS_ID), FUN = "length", drop = FALSE)
+  names(ae.num) = c("VAERS_ID", "ae_num")
+  tmp = merge(tmp, ae.num, by="VAERS_ID")
+  
+  dd_ae[[k]]=tmp
   dd_ae[[k]]$year=year[k]
 }
-data_AE=Reduce("rbind",dd_ae)
-data_AE=data_AE[order(data_AE$VAERS_ID),]
+data_ae=Reduce("rbind",dd_ae)
+data_ae=data_ae[order(data_ae$VAERS_ID),]
 
-ae_names = unique(data_AE$AE_NAME)
+#length(unique(data_ae$AE_NAME))  > 8592
 
-ddall = read.csv('dd.all.csv')
-ddall_ptlist = unique(ddall[,c('pt_name','pt')])
+meddra_ptlist = read.csv('meddra_ptlist.csv', header=T)
+meddra_ptlist = meddra_ptlist[complete.cases(meddra_ptlist),]
+data_AE = merge(data_ae, meddra_ptlist, by = 'AE_NAME', all.x = T)
 
-## Checking the number of unmatched Meddra PT terms
-length(ae_names) - sum(ae_names %in% ddall$pt_name)
+#length(unique(data_AE$MEDDRA_AE_NAME))  > 7788
+
+#### combine VAX-AE-PAT files
+dd_VAC_AE=merge(data_VAC,data_AE, by=c("VAERS_ID"))
+dd_VAC_AE_PAT=merge(dd_VAC_AE,data_PAT, by=c("VAERS_ID"), all.x = TRUE)
+dds=dd_VAC_AE_PAT[order(dd_VAC_AE_PAT$VAERS_ID),]
+
+dds = dds %>% select(VAERS_ID, MEDDRA_AE_NAME, MEDDRA_ID, VAX_NAME, VAX_TYPE, year = year.x, AGE, SEX, STATE, vac_num, ae_num) 
 
 
-########## Generating a table that has the information of MEDDRA PT terms and their MEDDRA ID
-meddra_ptlist = data.frame(matrix(NA, nrow = length(ae_names), ncol=4))
-colnames(meddra_ptlist) = c('AE_NAME', 'ID', 'MEDDRA_AE_NAME', 'MEDDRA_ID')
+write.csv(dds, 'full_dds.csv', row.names = F)
 
-### Step 1: Getting Meddra ID of the PT terms which already exist in the 'ddall.csv'
-meddra_ptlist$AE_NAME = ae_names
-meddra_ptlist$ID = ifelse(meddra_ptlist$AE_NAME %in% ddall_ptlist$pt_name,ddall_ptlist$pt, NA)
+#-- Summary and check
+length(unique(dds$AE_NAME[is.na(dds$MEDDRA_ID)]))  ### only 413 AE names are unmatched. + NA = 413
+length(unique(data_ae$AE_NAME[is.na(data_ae$ID)]))
 
-### Step 2: Getting Meddra ID of the PT terms which are not matched with 'ddall.csv'
-## Webscraping using 'RESTful API': Getting Jason data and extracting Meddra information
+length(unique(dds$AE_NAME))
+length(unique(dds$MEDDRA_AE_NAME))
 
-idx = which(is.na(meddra_ptlist$ID))
-cnt = 0
-for(i in idx){
-  try({
-    x = meddra_ptlist[i,'AE_NAME']
-    url = paste('http://data.bioontology.org/search?apikey=cab5a7da-2d12-4391-9de6-90e6b4c71580&q=',
-                gsub(' ', x, replacement = '%20'), '%7D', sep='')
-    ae_url = fromJSON(url)$collection
-    m_idx = (ae_url$prefLabel==x & grepl('MEDDRA',ae_url$`@id`))
-    label_id = ae_url[m_idx,"@id"]
-    meddra_ptlist[i,'ID'] = str_extract(label_id,'100[0-9]*')
-  })
-  cnt = cnt + 1
-  print(cnt)
-}
+dds[is.na(dds$vaxnum),]
 
-### Step 3: Getting Formal Meddra PT names and their Meddra ID
+dds[is.na(dds$year),] ### Meaning that these VAERS_IDs do not exist on data_VAC or data_PAT, but only on data_AE
 
-# Modified strsplit function for convenience
-strsplit2 <- function(x,split,type = "remove", perl = FALSE, ...) {
-  if (type == "remove") {
-    out <- base::strsplit(x = x, split = split, perl = perl, ...)
-  } else if (type == "before") {
-    out <- base::strsplit(x = x, split = paste0("(?<=.)(?=", split, ")"),perl = TRUE,...)
-  } else if (type == "after") {
-    out <- base::strsplit(x = x, split = paste0("(?<=", split, ")"), perl = TRUE,...)
-  } else {
-    stop("type must be remove, after or before!")}
-  return(out)
-}
-
-## Webscraping from BioPortal HTML sources 
-cnt = 0
-for(i in which(!is.na(meddra_ptlist$ID))){
-  try({ ##Getting "Classified_as" and its ID
-    url_c = paste('http://purl.bioontology.org/ontology/MEDDRA/', meddra_ptlist[i,'ID'], sep='')
-    read = read_html(url_c)
-    node = html_nodes(read, "td")
-    text = as.character(html_text(node))
-    
-    classifiED = gsub('\n', replacement='', x=text[which(tolower(text) == "classified as") + 1])
-    classifiED = unlist(strsplit2(classifiED, 'http://', type="before"))
-    
-    
-    if(length(classifiED) > 0){
-      vec_ED = NULL; vec_ED_id = NULL
-      for(d in 1:length(classifiED)){
-        page_ED = read_html(classifiED[d])
-        node_ED = html_nodes(page_ED, "td")
-        text_ED = as.character(html_text(node_ED))
-        vec_ED = c(vec_ED,
-                   gsub('\n', replacement='', x=text_ED[which(tolower(text_ED) == "preferred name") + 1]))
-        vec_ED_id = c(vec_ED_id,
-                      gsub('\n', replacement='', x=text_ED[which(tolower(text_ED) == "notation") + 1]))
-      }
-      meddra_ptlist[i,"MEDDRA_AE_NAME"] = paste(vec_ED, collapse = ", ")
-      meddra_ptlist[i,"MEDDRA_ID"] = paste(vec_ED_id, collapse = ", ")
-    }
-  })
-  cnt = cnt+1
-  if(cnt%%10==0){print(cnt)} #8179
-}
-
-idx2 = which(is.na(meddra_ptlist$MEDDRA_AE_NAME))
-meddra_ptlist[idx2, c("MEDDRA_AE_NAME","MEDDRA_ID")] = meddra_ptlist[idx2, c("AE_NAME","ID")]
-
-write.csv(meddra_ptlist,'meddra_ptlist.csv', row.names = F)
